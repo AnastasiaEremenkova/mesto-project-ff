4db006a55991960f3f5db5b0961095b63ba0ce45
Anastasia Eremenkova <eremenkovaanastasija@yandex.ru>
2024-05-21 12:16:25 +0300

    изменения

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cards = document.querySelector('.places__list');
+
+initialCards.forEach((cardInformation)=> {
+   const cardElement = cardSight(cardInformation, deleteCard);
+   cards.append(cardElement);
+
+});
+
+
+function cardSight(cardInformation, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardForm = cardTemplate.querySelector('.card');
+    const cardElement = cardForm.cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.alt = cardInformation.alt;
+    cardImage.src = cardInformation.link;
+    const cardTitle = cardForm.querySelector('.card__title');
+    cardTitle.textContent = cardInformation.name;
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', deleteCard);
+
+    return cardElement;
+};
+
+function deleteCard(event) {
+    event.target.closest('.card').remove()
+};
